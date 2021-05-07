35fc0b6e0475ad8cc72a149b7e730573933b239b
Michael Berger <mberger718@aol.com>
2021-05-07 10:33:50 -0400

    First Commit for FunctPractice.py

---
+++ FunctPractice.py
@@ -0,0 +1,12 @@
+def alphabetize(original_list=[]):
+    """Pass any list in square brackets"""
+    sorted_list = original_list.copy()
+    sorted_list.sort()
+    final_list = ''
+    for name in sorted_list:
+        final_list += name + ', '
+    final_list = final_list[:-2]
+    print(final_list)
+
+names = ['Lee', 'Andy', 'Mike']
+alphabetize(names)
