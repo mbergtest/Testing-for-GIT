9fed9d12494c0232737e1990d5d2e90d4523a535
Michael Berger <mberger718@aol.com>
2021-05-12 13:52:55 -0400

    added different line 21 to branch1

--- FunctPractice.py
+++ FunctPractice.py
@@ -18,3 +18,4 @@ alphabetize(names)
 
 #Added a comment at the end of the branched version, then merged to master
 #Added another comment at the end of the branched version
+#Added a different line 21 to branch1 version
